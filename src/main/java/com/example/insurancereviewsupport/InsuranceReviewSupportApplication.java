567a4a50e3b13251e4b3a6bc57c3bd264b96b0ec
nasihyeon <nasihyun77@gmail.com>
2026-09-13 15:54:00 +0900

    chore: initialize project

---
+++ src/main/java/com/example/insurancereviewsupport/InsuranceReviewSupportApplication.java
@@ -0,0 +1,13 @@
+package com.example.insurancereviewsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceReviewSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceReviewSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/insurancereviewsupport/InsuranceReviewSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurancereviewsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceReviewSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
